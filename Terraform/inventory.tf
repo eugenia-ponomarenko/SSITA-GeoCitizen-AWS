d33d8500e2a9bb5a3aa1aae476a47190f0de4247
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-03-28 19:25:32 +0300

    Update inventory.tf

--- Terraform/inventory.tf
+++ Terraform/inventory.tf
@@ -1,9 +1,3 @@
-resource "local_file" "private_key" {
-  sensitive_content = tls_private_key.key.private_key_pem
-  filename          = format("%s/%s/%s", abspath(path.root), ".ssh", "ssh-key.pem")
-  file_permission   = "0600"
-}
-
 resource "local_file" "ansible_inventory" {
   filename = format("%s/%s/%s", abspath(path.root), "inventory", "inventory.yaml")
   file_permission   = "0600"
@@ -13,7 +7,7 @@ ubuntu_server:
     ${aws_instance.ubuntu_web_server.public_ip} 
   vars:
     ansible_ssh_user: ubuntu
-    ansible_ssh_private_key_file: ${local_file.private_key.filename}
+    ansible_ssh_private_key_file: 
     ansible_python_interpreter: /usr/bin/python3
 EOF
 }
