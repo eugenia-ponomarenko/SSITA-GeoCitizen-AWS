a45207923e8716a4ce9531be328f415aa98d3c08
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-03-29 16:34:54 +0300

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -4,11 +4,6 @@ locals {
   instance_type = "t2.micro"
 }
 
-variable "ec2_ports" {
-  type    = list(number)
-  default = [22, 8080, 587]
-}
-
 provider "aws" {
   region  = "eu-central-1"
   profile = "default"
@@ -29,26 +24,6 @@ resource "aws_instance" "ubuntu_web_server" {
 
 }
 
-resource "aws_security_group" "ubuntuSecurityGroup" {
-  name        = "WebServer Security Group"
-  description = "GeoCitizen. SecurityGroup for Ubuntu"
-  dynamic "ingress" {
-    for_each = var.ec2_ports
-    content {
-      from_port        = ingress.value
-      to_port          = ingress.value
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 # ----------------------------------------------
 # ------------------ RDS -----------------------
@@ -63,58 +38,4 @@ resource "aws_db_instance" "GeoCitizenDB" {
   parameter_group_name   = "default.postgres12"
   skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.RDS_SecurityGroup.id]
-}
-
-resource "aws_security_group" "RDS_SecurityGroup" {
-  name        = "PostgreSQL Security Group"
-  description = "GeoCitizen. SecurityGroup for RDS"
-
-  ingress {
-    description = "PostgreSQL"
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-
-# -----------------------------------------------
-# ------------------- IAM -----------------------
-
-resource "aws_iam_role" "geocit_accessToRDS" {
-  name = "Geocit-AccessToRDS"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_policy_attachment" "attach_policy" {
-  name       = "policy_attachment"
-  roles      = ["${aws_iam_role.geocit_accessToRDS.name}"]
-  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
-}
-
-resource "aws_iam_instance_profile" "geocit_profile" {
-  name = "geocitizen"
-  role = aws_iam_role.geocit_accessToRDS.name
 }
