1c0d2c25850e255d676b04e129fffe35efea2cee
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-03-29 16:34:21 +0300

    Create securityGroups.tf

---
+++ Terraform/securityGroups.tf
@@ -0,0 +1,45 @@
+variable "ec2_ports" {
+  type    = list(number)
+  default = [22, 8080, 587]
+}
+
+resource "aws_security_group" "ubuntuSecurityGroup" {
+  name        = "WebServer Security Group"
+  description = "GeoCitizen. SecurityGroup for Ubuntu"
+  dynamic "ingress" {
+    for_each = var.ec2_ports
+    content {
+      from_port        = ingress.value
+      to_port          = ingress.value
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "RDS_SecurityGroup" {
+  name        = "PostgreSQL Security Group"
+  description = "GeoCitizen. SecurityGroup for RDS"
+
+  ingress {
+    description = "PostgreSQL"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
