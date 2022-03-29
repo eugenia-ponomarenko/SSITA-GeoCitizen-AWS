e72dbb8ae7e5c35745f11173a418ac054d364016
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-03-29 16:35:19 +0300

    Create iamRole.tf

---
+++ Terraform/iamRole.tf
@@ -0,0 +1,29 @@
+resource "aws_iam_role" "geocit_accessToRDS" {
+  name = "Geocit-AccessToRDS"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy_attachment" "attach_policy" {
+  name       = "policy_attachment"
+  roles      = ["${aws_iam_role.geocit_accessToRDS.name}"]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+}
+
+resource "aws_iam_instance_profile" "geocit_profile" {
+  name = "geocitizen"
+  role = aws_iam_role.geocit_accessToRDS.name
+}
