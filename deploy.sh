db52bf05586839523cc4f371348deec127eb68e1
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-03-29 19:57:43 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,6 @@
 #!/bin/sh
 
-sudo rm -rf ~/Geocit134
-cd ~/
-git clone https://github.com/mentorchita/Geocit134.git
-cd ~/Geocit134
-
 #----------------------------------------------------------------------------------------------------
-# Remove old project files from tomcat
-
-sudo rm -rf /usr/share/tomcat/webapps/citizen.war
-sudo rm -rf /usr/share/tomcat/webapps/citizen
-
-# #----------------------------------------------------------------------------------------------------
-# # Fix pom.xml
-
-# sed -i "s/http:\/\/repo.spring.io/https:\/\/repo.spring.io/g" ~/Geocit134/pom.xml
-# sed -i "s/>servlet-api</>javax.servlet-api</g" ~/Geocit134/pom.xml
-# sed -i "s/<distributionManagement>/<\!--\n<distributionManagement>/g" ~/Geocit134/pom.xml
-# sed -i "s/<\/distributionManagement>/<\/distributionManagement>\n-->/g" ~/Geocit134/pom.xml
-
-# #----------------------------------------------------------------------------------------------------
 # Update email credentials
 
 . ~/emailCredentials
@@ -49,12 +30,12 @@ sed -i "s/$old_dbip/$new_dbip/g" ~/Geocit134/src/main/resources/application.prop
 # Correct path to js directory
 
 sed -i "s/\/src\/assets/\.\/static/g" ~/Geocit134/src/main/webapp/index.html
-
+# /src/assets / ./static
 #-----------------------------------------------------------------------------------------------------
 # build and deploy
 
-mvn install
-sleep 5
-sudo mv target/citizen.war /usr/share/tomcat/webapps/ 
-sleep 5
-sudo sh /usr/share/tomcat/bin/startup.sh
+# mvn install
+# sleep 5
+# sudo mv target/citizen.war /usr/share/tomcat/webapps/ 
+# sleep 5
+# sudo sh /usr/share/tomcat/bin/startup.sh
