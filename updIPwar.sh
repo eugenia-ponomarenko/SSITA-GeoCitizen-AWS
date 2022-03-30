dec487cbe9bcddafe09cef9ff781960f836d3ac2
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-03-30 22:57:45 +0300

    Create updIPwar.sh

---
+++ updIPwar.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sudo su -
+cd /usr/share/tomcat/webapps
+
+vm_host=
+db_host=
+old_serverip="localhost\|[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}"
+old_dbip="postgresql:\/\/[a-zA-Z0-9.-]*:5432"
+new_dbip="postgresql:\/\/$db_host"
+
+sed -i "s/$old_serverip/$vm_host/g" ./citizen/WEB-INF/classes/com/softserveinc/geocitizen/configuration/MongoConfig.class
+sed -i "s/$old_serverip/$vm_host/g" ./citizen/static/js/*
+sed -i "s/$old_serverip/$vm_host/g" ./citizen/WEB-INF/classes/application.properties  
+sed -i "s/$old_dbip/$new_dbip/g" ./citizen/WEB-INF/classes/application.properties
