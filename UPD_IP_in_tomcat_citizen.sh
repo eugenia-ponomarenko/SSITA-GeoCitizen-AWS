f4784f0e8dd8079ebe6a9ee99a14f1205165c305
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-01 20:26:21 +0300

    Update UPD_IP_in_tomcat_citizen.sh

--- UPD_IP_in_tomcat_citizen.sh
+++ UPD_IP_in_tomcat_citizen.sh
@@ -6,10 +6,12 @@ cd /usr/share/tomcat/webapps
 vm_host=
 postgres_db_host=
 old_serverip="localhost\|[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}"
-old_dbip="postgresql:\/\/[a-zA-Z0-9.-]*:5432\/ss_demo_1"
-new_dbip="postgresql:\/\/$postgres_db_host\/ss_demo_1"
+old_dbip="postgresql:\/\/[a-zA-Z0-9.-]*:5432"
+new_dbip="postgresql:\/\/$postgres_db_host"
 
 sed -i "s/$old_serverip/$vm_host/g" ./citizen/WEB-INF/classes/com/softserveinc/geocitizen/configuration/MongoConfig.class
 sed -i "s/$old_serverip/$vm_host/g" ./citizen/static/js/*
 sed -i "s/$old_serverip/$vm_host/g" ./citizen/WEB-INF/classes/application.properties  
 sed -i "s/$old_dbip/$new_dbip/g" ./citizen/WEB-INF/classes/application.properties
+sed -i "s/5432\n\/ss_dem_1/5432\/ss_demo_1/g" ./src/main/resources/application.properties
+sed -i "s/5432\n\/ss_dem_1_test/5432\/ss_demo_1_test/g" ./src/main/resources/application.properties
