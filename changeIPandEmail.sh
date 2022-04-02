77642001a605477441fbe8ee5c2eaa759c7636e8
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-02 15:54:44 +0300

    Update changeIPandEmail.sh

--- changeIPandEmail.sh
+++ changeIPandEmail.sh
@@ -15,10 +15,10 @@ sed -i "s/$old_passwd/$new_passwd/g" ./src/main/resources/application.properties
 #----------------------------------------------------------------------------------------------------
 # Update ip addresses
 
-. ./Terraform/details/credentials.sh
+. ./Terraform/details/credentials
 
 old_serverip="localhost\|[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}"
-old_dbip="postgresql:\/\/[a-zA-Z0-9.-]*:5432"
+old_dbip="postgresql:\/\/[a-zA-Z0-9.-]*:5432\/ss_demo_1"
 
 sed -i "s/$old_serverip/$ubuntu_host/g" ./src/main/java/com/softserveinc/geocitizen/configuration/MongoConfig.java   
 sed -i "s/$old_serverip/$ubuntu_host/g" ./src/main/webapp/static/js/*
@@ -26,4 +26,3 @@ sed -i "s/$old_serverip/$ubuntu_host/g" ./src/main/resources/application.propert
 sed -i "s/$old_dbip/postgresql:\/\/$db_host\/ss_demo_1/g" ./src/main/resources/application.properties
 # sed -i "s/5432\n\/ss_dem_1/5432\/ss_demo_1/g" ./src/main/resources/application.properties
 # sed -i "s/5432\n\/ss_dem_1_test/5432\/ss_demo_1_test/g" ./src/main/resources/application.properties
-
