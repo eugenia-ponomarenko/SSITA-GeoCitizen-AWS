0841b9b491b0cff2585ca4e0c396ab977639b539
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-02 15:53:17 +0300

    Update inventory.tf

--- Terraform/inventory.tf
+++ Terraform/inventory.tf
@@ -12,10 +12,9 @@ ubuntu_server:
 EOF
 }
 resource "local_file" "credentials" {
-  filename = format("%s/%s/%s", abspath(path.root), "details", "credentials.sh")
+  filename = format("%s/%s/%s", abspath(path.root), "details", "credentials")
   file_permission   = "0600"
-  content = <<EOF
-ubuntu_host="${aws_instance.u_web_server.public_ip}"
-db_host="${aws_db_instance.GeoCitDB.endpoint}"
-EOF
+  content = <<EOL
+ubuntu_host="${aws_instance.u_web_server.public_ip}" db_host="${aws_db_instance.GeoCitDB.endpoint}"
+EOL
 }
