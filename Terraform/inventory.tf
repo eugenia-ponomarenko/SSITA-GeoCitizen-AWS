fe7d145859b82639dcd17b99315d0fbf1ab76bd3
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-02 15:48:24 +0300

    Update inventory.tf

--- Terraform/inventory.tf
+++ Terraform/inventory.tf
@@ -4,7 +4,7 @@ resource "local_file" "ansible_inventory" {
   content = <<EOF
 ubuntu_server:
   hosts:
-    ${aws_instance.u_web_server.public_ip} 
+    ${aws_instance.u_web_server.public_ip}
   vars:
     ansible_ssh_user: ubuntu
     ansible_ssh_private_key_file: ../.ssh/ansible_ssh_key.pem
@@ -14,8 +14,8 @@ EOF
 resource "local_file" "credentials" {
   filename = format("%s/%s/%s", abspath(path.root), "details", "credentials.sh")
   file_permission   = "0600"
-  content = <<EOF 
-ubuntu_host="${aws_instance.u_web_server.public_ip}" 
+  content = <<EOF
+ubuntu_host="${aws_instance.u_web_server.public_ip}"
 db_host="${aws_db_instance.GeoCitDB.endpoint}"
 EOF
 }
