19e0124b5539f0c21524734e28d1fb5838698c5a
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-12 17:26:43 +0300

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,10 +1,18 @@
 locals {
-  key_name = "ansible_ssh_key"
-  ami_id = "ami-042ad9eec03638628"  # Ubuntu Server 18.04 LTS (HVM)
-  instance_type = "t2.micro"
-  db_name = "PostgreSQL_GeoCitizen"
-  vm_name = "Ubuntu_WebServer"
-  webserver_security_group = "GeoCitizen WebServer Security Group"
-  postgres_security_group = "RDS PostgreSQL Security Group"
+  key_name                  = "ansible_ssh_key"
+  ami_id                    = "ami-042ad9eec03638628"  # Ubuntu Server 18.04 LTS (HVM)
+  instance_type             = "t2.micro"
+  db_name                   = "PostgreSQL_GeoCitizen"
+  vm_name                   = "Ubuntu_WebServer"
+  webserver_security_group  = "GeoCitizen WebServer Security Group"
+  postgres_security_group   = "RDS PostgreSQL Security Group"
+  iam_role_name             = "GeoCitizen-AccessToRDS"
+  iam_policy_att_name       = "policy_attachment"
+  policy_arn                = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+  iam_instance_profile_name = "GeoCitizen"
 }
 
+variable "ec2_ports" {
+  type    = list(number)
+  default = [22, 8080, 587]
+}
