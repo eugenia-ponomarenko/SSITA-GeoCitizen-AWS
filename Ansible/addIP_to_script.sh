fdeb8ebdd86d0fe2fac062961f43040337a7db48
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-13 11:52:18 +0300

    Delete addIP_to_script.sh

--- Ansible/addIP_to_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-. ../Terraform/details/credentials
-
-sed -i "s/postgres_db_host=/postgres_db_host=\'$db_host\'/g" ./UPD_IP_in_tomcat_citizen.sh
-sed -i "s/vm_host=/vm_host=\'$ubuntu_host\'/g" ./UPD_IP_in_tomcat_citizen.sh
