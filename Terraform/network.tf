0863459c2d5b23be0e57882e597d816508d05b5a
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-13 23:15:24 +0300

    Update network.tf

--- Terraform/network.tf
+++ Terraform/network.tf
@@ -19,27 +19,6 @@ resource "aws_security_group" "ubuntuSecurityGroup" {
   }
 }
 
-# resource "aws_security_group" "RDS_SecurityGroup" {
-#   name        = local.postgres_security_group
-#   description = "GeoCitizen. SecurityGroup for RDS"
-
-#   ingress {
-#     description = "PostgreSQL"
-#     from_port   = 5432
-#     to_port     = 5432
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-
-#   egress {
-#     from_port   = 0
-#     to_port     = 0
-#     protocol    = "-1"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
-
-
 # --------------------------------------------
 # Create AWS Application Load Balancer for Web
 
