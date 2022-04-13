3bddf1fe3be25fcac3752cccabe960e68b71bda8
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-13 14:41:32 +0300

    Update iamRole.tf

--- Terraform/iamRole.tf
+++ Terraform/iamRole.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "geocit_accessToRDS" {
+resource "aws_iam_role" "GeoCit_Role" {
   name = local.iam_role_name
   assume_role_policy = <<EOF
 {
@@ -17,13 +17,31 @@ resource "aws_iam_role" "geocit_accessToRDS" {
 EOF
 }
 
+resource "aws_iam_policy" "policy" {
+  name   = "RDSAndS3FullAcess"
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:*",
+                "s3-object-lambda:*",
+                "rds:*"
+            ],
+            "Resource": "*"
+        }
+    ]
+  })
+}
+
 resource "aws_iam_policy_attachment" "attach_policy" {
   name       = local.iam_policy_att_name
-  roles      = ["${aws_iam_role.geocit_accessToRDS.name}"]
-  policy_arn = local.policy_arn
+  roles      = [aws_iam_role.GeoCit_Role.name]
+  policy_arn = aws_iam_policy.policy.arn
 }
 
 resource "aws_iam_instance_profile" "geocit_profile" {
   name = local.iam_instance_profile_name
-  role = aws_iam_role.geocit_accessToRDS.name
+  role = aws_iam_role.GeoCit_Role.name
 }
