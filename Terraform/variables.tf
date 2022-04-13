b3407c738dcfa48156d733f864175514c0e8458e
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-13 17:31:15 +0300

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,7 +1,6 @@
 locals {
   ami_id                    = "ami-042ad9eec03638628"  # Ubuntu Server 18.04 LTS (HVM)
-  instance_type             = "t2.micro"  
-  policy_arn                = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+  instance_type             = "t2.micro"
   availability_zones        = ["eu-central-1a", "eu-central-1b"]
   eu_central_1ab            = ["subnet-0bd44ce4febe6eacb", "subnet-0f13e4432ca16d78a"]
   key_name                  = "geocit-app"
