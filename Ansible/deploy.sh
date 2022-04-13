042de4e9eeb619378f1bc39444dbc01b7a0e86e4
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-13 22:45:03 +0300

    Update deploy.sh

--- Ansible/deploy.sh
+++ Ansible/deploy.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
+# -------------------------------------------------------------------------------------
+# variables
 
-tomcat_path="/usr/share/tomcat"
+tomcat_path="/opt/tomcat/latest"
 
-db_host="terraform-20220402130437975900000001"
+old_serverip="localhost\|[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}"
+vm_host=$(curl --silent --url "www.ifconfig.me" | tr "\n" " ")
+
+db_host="lb-geocitizen.cstswihxzihx.eu-central-1.rds.amazonaws.com"
 
 old_dbip="postgresql:\/\/[a-zA-Z0-9.-]*:5432"
-new_dbip="postgresql:\/\/$db_host"
+new_dbip="postgresql:\/\/$db_host:5432"
 
-vm_host=$(curl --silent --url "www.ifconfig.me" | tr "\n" " ")
-old_serverip="localhost\|[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}"
-
-# --------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------
 # Start RDS instance
 aws configure set default.region eu-central-1
-aws rds start-db-instance --db-instance-identifier $db_host
+aws rds start-db-instance --db-instance-identifier lb-geocitizen
 
+# -------------------------------------------------------------------------------------
 # Download citizen.war from S3 
 aws s3 cp s3://geo-citizen-war/geo-citizen-1.0.5-20220404.173539-1.war  ~/citizen.war
 
@@ -30,4 +33,4 @@ sudo sed -i "s/$old_serverip/$vm_host/g" $tomcat_path/webapps/citizen/static/js/
 sudo sed -i "s/$old_serverip/$vm_host/g" $tomcat_path/webapps/citizen/WEB-INF/classes/application.properties
 
 # Fix IPs for db
-sudo sed -i "s/$old_dbip/$new_dbip/g" $tomcat_path/webapps/citizen/WEB-INF/classes/application.properties
+sudo sed -i "s/$old_dbip/$new_dbip/g"    $tomcat_path/webapps/citizen/WEB-INF/classes/application.properties
