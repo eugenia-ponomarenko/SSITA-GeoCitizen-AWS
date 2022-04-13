453957df2ab5f53422c613eec047668ca0e8c225
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-13 16:45:48 +0300

    Update and rename main.tf to rds.tf

--- Terraform/rds.tf
+++ Terraform/rds.tf
@@ -1,14 +1,3 @@
-resource "aws_instance" "u_web_server" {
-  ami                    = local.ami_id
-  instance_type          = local.instance_type
-  vpc_security_group_ids = [aws_security_group.ubuntuSecurityGroup.id]
-  iam_instance_profile   = aws_iam_instance_profile.geocit_profile.name
-  key_name               = local.key_name
-  tags = {
-    Name = local.vm_name
-  }
-}
-
 resource "aws_db_instance" "PostgresDB" {
   allocated_storage      = 10
   engine                 = "postgres"
@@ -20,7 +9,4 @@ resource "aws_db_instance" "PostgresDB" {
   parameter_group_name   = "default.postgres12"
   skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.RDS_SecurityGroup.id]
-   tags = {
-    Name = local.db_name
-  }
 }
