7f7f997f34aa639bb79984eecbc0aed16c722a10
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-13 19:06:43 +0300

    Update network.tf

--- Terraform/network.tf
+++ Terraform/network.tf
@@ -19,25 +19,25 @@ resource "aws_security_group" "ubuntuSecurityGroup" {
   }
 }
 
-resource "aws_security_group" "RDS_SecurityGroup" {
-  name        = local.postgres_security_group
-  description = "GeoCitizen. SecurityGroup for RDS"
+# resource "aws_security_group" "RDS_SecurityGroup" {
+#   name        = local.postgres_security_group
+#   description = "GeoCitizen. SecurityGroup for RDS"
 
-  ingress {
-    description = "PostgreSQL"
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+#   ingress {
+#     description = "PostgreSQL"
+#     from_port   = 5432
+#     to_port     = 5432
+#     protocol    = "tcp"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
 
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
+#   egress {
+#     from_port   = 0
+#     to_port     = 0
+#     protocol    = "-1"
+#     cidr_blocks = ["0.0.0.0/0"]
+#   }
+# }
 
 
 # --------------------------------------------
