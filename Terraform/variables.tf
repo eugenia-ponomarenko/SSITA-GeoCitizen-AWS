72465ebd986960505b7cab69a55f7aa6964b889d
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-13 16:38:40 +0300

    Update variables.tf

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -1,16 +1,21 @@
 locals {
-  key_name                  = "geocit-app"
   ami_id                    = "ami-042ad9eec03638628"  # Ubuntu Server 18.04 LTS (HVM)
-  instance_type             = "t2.micro"
+  instance_type             = "t2.micro"  
+  policy_arn                = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+  availability_zones        = ["eu-central-1a", "eu-central-1b"]
+  eu_central_1ab            = ["subnet-0bd44ce4febe6eacb", "subnet-0f13e4432ca16d78a"]
+  key_name                  = "geocit-app"
+  lb_name                   = "TF-GeoCitizen-LB"
+  asg_name                  = "AS_TF_WebServer"
+  lt_name                   = "WebServerTomcat"
   db_name                   = "PostgreSQL_GeoCitizen"
   vm_name                   = "Ubuntu_WebServer"
   webserver_security_group  = "Ubuntu Security Group"
   postgres_security_group   = "Postgres Security Group"
+  lb_security_group         = "GeoCitizen_LB"
   iam_role_name             = "AccessToRDS"
   iam_policy_att_name       = "policy_attachment"
-  policy_arn                = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
   iam_instance_profile_name = "GC-profile"
-  availability_zones        = ["eu-central-1a", "eu-central-1b"]
 }
 
 variable "ec2_ports" {
