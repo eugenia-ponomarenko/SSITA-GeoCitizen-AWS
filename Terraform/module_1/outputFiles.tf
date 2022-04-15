9e0183aa91684542cc9c52162ebc0636c9397ce1
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-15 23:58:52 +0300

    Update outputFiles.tf

--- Terraform/module_1/outputFiles.tf
+++ Terraform/module_1/outputFiles.tf
@@ -1,26 +1,3 @@
-resource "local_file" "user_data" {
-  filename = format("%s/%s", "../module_2", "user_data.sh")
-  file_permission   = "0755"
-  content = <<EOF
-#!/bin/bash
-sudo apt-add-repository ppa:ansible/ansible;
-sudo apt update;
-sudo apt install -y awscli ansible git;
-# Start RDS and download citizen.war from S3
-aws configure set default.region eu-central-1;
-aws rds start-db-instance --db-instance-identifier lb-geocitizen;
-BUCKET=geo-citizen-war;
-KEY=`aws s3 ls $BUCKET --recursive | sort | tail -n 1 | awk '{print $4}'`; 
-aws s3 cp s3://$BUCKET/$KEY ~/citizen.war;
-sudo mv ~/citizen.war /usr/share/tomcat/webapps/citizen.war;
-# Get LB DNS;
-vm_host="${aws_lb.tf_lb_webserver.dns_name}"
-# Run Ansible playbook
-git clone -b lg_asg_test https://github.com/eugenia-ponomarenko/SSITA-GeoCitizen-AWS.git ~/lb_asg;
-ansible-playbook ~/lb_asg/Ansible/play.yml;
-EOF
-}
-
 resource "local_file" "tg_arn" {
   filename = format("%s/%s", "../module_2", "var.tf")
   file_permission   = "0755"
@@ -28,8 +5,6 @@ resource "local_file" "tg_arn" {
 variable target_group_arn { default = "${aws_lb_target_group.target_group.arn}" }
 EOF
 }
-
-
 resource "local_file" "credentials" {
   filename = format("%s/%s", "../", "credentials")
   file_permission   = "0600"
