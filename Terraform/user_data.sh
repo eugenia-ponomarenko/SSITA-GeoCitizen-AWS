5c60996fe885e3645a5058a6b55eca826420bff2
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-15 14:24:35 +0300

    Update user_data.sh

--- Terraform/user_data.sh
+++ Terraform/user_data.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-
 sudo apt-add-repository ppa:ansible/ansible;
 sudo apt update;
-sudo apt install ansible -y;
-sudo apt install git -y;
-git clone -b lb_asg_tf_ansible https://github.com/eugenia-ponomarenko/SSITA-GeoCitizen-AWS.git ~/lb_asg_tf_ansible;
-ansible-playbook ~/lb_asg_tf_ansible/Ansible/play.yml
+sudo apt install -y awscli ansible git;
+# Start RDS and download citizen.war from S3
+aws configure set default.region eu-central-1;
+aws rds start-db-instance --db-instance-identifier lb-geocitizen;
+BUCKET=geo-citizen-war;
+KEY=`aws s3 ls $BUCKET --recursive | sort | tail -n 1 | awk '{print $4}'`; 
+aws s3 cp s3://$BUCKET/$KEY ~/citizen.war;
+sudo cp ~/citizen.war /usr/share/tomcat/webapps/citizen.war;
+# Run Ansible playbook
+git clone -b lg_asg_test https://github.com/eugenia-ponomarenko/SSITA-GeoCitizen-AWS.git ~/lb_asg;
+ansible-playbook ~/lb_asg/Ansible/play.yml;
