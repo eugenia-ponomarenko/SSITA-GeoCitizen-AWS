78586770060e4da16ceec5d689faf92619435ad6
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-15 22:52:01 +0300

    Create local.tf

---
+++ Terraform/module_1/local.tf
@@ -0,0 +1,12 @@
+locals {
+  eu_central_1ab            = ["subnet-0bd44ce4febe6eacb", "subnet-0f13e4432ca16d78a"]
+  lb_name                   = "TF-GeoCitizen-LB"
+  webserver_security_group  = "Ubuntu Security Group"
+  vpc_id                    = "vpc-008b05236db3e2d5e"
+  lb_security_group         = "GeoCitizen_LB"
+}
+
+variable "ec2_ports" {
+  type    = list(number)
+  default = [22, 8080, 587]
+}
