5bd43ff28292d105abcbf7bec2a19a919ca91505
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-15 23:32:21 +0300

    Create rds.tf

---
+++ Terraform/module_1/rds.tf
@@ -0,0 +1,35 @@
+resource "aws_db_instance" "GeoCitDB" {
+  allocated_storage      = 10
+  engine                 = "postgres"
+  engine_version         = "12.9"
+  instance_class         = "db.t2.micro"
+  db_name                = "ss_demo_1"
+  username               = "postgres"
+  password               = "postgres"
+  parameter_group_name   = "default.postgres12"
+  skip_final_snapshot    = true
+  vpc_security_group_ids = [aws_security_group.RDS_SecurityGroup.id]
+   tags = {
+    Name = local.db_name
+  }
+}
+
+resource "aws_security_group" "RDS_SecurityGroup" {
+  name        = local.postgres_security_group
+  description = "GeoCitizen. SecurityGroup for RDS"
+
+  ingress {
+    description = "PostgreSQL"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
