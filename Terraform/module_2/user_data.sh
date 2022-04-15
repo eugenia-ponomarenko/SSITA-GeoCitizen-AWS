2b686d916c91ee58161e2e789cdd0013c7b88c1f
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-15 23:59:26 +0300

    Create user_data.sh

---
+++ Terraform/module_2/user_data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt-add-repository ppa:ansible/ansible;
+sudo apt update;
+sudo apt install -y awscli ansible git;
+# Download citizen.war from S3
+aws configure set default.region eu-central-1;
+aws s3 cp s3://geo-citizen-war/citizen.war ~/citizen.war;
+# Run Ansible playbook
+git clone -b lg_test https://github.com/eugenia-ponomarenko/SSITA-GeoCitizen-AWS.git ~/lb_asg;
+ansible-playbook ~/lb_asg/Ansible/play.yml;
