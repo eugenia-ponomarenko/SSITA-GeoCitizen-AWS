bdd014bf38b2a085adc25a6db8eedc2b8dd38aa2
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-16 23:48:31 +0300

    Update changeEmailAndIP.sh

--- changeEmailAndIP.sh
+++ changeEmailAndIP.sh
@@ -20,8 +20,7 @@ sed -i "s/$old_passwd/$new_passwd/g" ./src/main/resources/application.properties
 old_serverip="localhost\|[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}"
 old_dbip="postgresql:\/\/[a-zA-Z0-9.-]*:5432\/ss_demo_1"
 new_dbip="postgresql:\/\/$db_host\/ss_demo_1"
-
-sed -i "s/$old_serverip/$lb_dns/g" ./src/main/java/com/softserveinc/geocitizen/configuration/MongoConfig.java   
+ 
 sed -i "s/$old_serverip/$lb_dns/g" ./src/main/webapp/static/js/*
 sed -i "s/$old_serverip/$lb_dns/g" ./src/main/resources/application.properties
 
