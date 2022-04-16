887edf821aacb9039d56a62611ff7985bdf49820
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-17 02:18:02 +0300

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -20,8 +20,8 @@ sed -i "s/$old_passwd/$new_passwd/g" ./src/main/resources/application.properties
 ##################Adjusting_application.properties###############################
 sed -i -E \
             "s/(http:\/\/localhost:8080)/http:\/\/$lb_dns:80/g; \
-            s/(postgresql:\/\/localhost)/postgresql:\/\/$db_host/g;
-            s/(35.204.28.238)/$db_host/g; " src/main/resources/application.properties
+            s/(postgresql:\/\/localhost:5432)/postgresql:\/\/$db_host/g;
+            s/(35.204.28.238:5432)/$db_host/g; " src/main/resources/application.properties
 
 ##################Repair index.html favicon###############################
 sed -i "s/\/src\/assets/\.\/static/g" src/main/webapp/index.html
