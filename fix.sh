39b8857dcaebff20ef142a0bef00daa1b423d8d9
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-17 02:02:55 +0300

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -19,12 +19,12 @@ sed -i "s/$old_passwd/$new_passwd/g" ./src/main/resources/application.properties
 
 ##################Adjusting_application.properties###############################
 sed -i -E \
-            "s/(http:\/\/localhost:8080)/http:\/\/${lb_dns}:80/g; \
-            s/(postgresql:\/\/localhost)/postgresql:\/\/${db_host}/g;
-            s/(35.204.28.238)/${db_host}/g; " src/main/resources/application.properties
+            "s/(http:\/\/localhost:8080)/http:\/\/$lb_dns:80/g; \
+            s/(postgresql:\/\/localhost)/postgresql:\/\/$db_host/g;
+            s/(35.204.28.238)/$db_host/g; " src/main/resources/application.properties
 
 ##################Repair index.html favicon###############################
 sed -i "s/\/src\/assets/\.\/static/g" src/main/webapp/index.html
 ##################Repair js bundles###############################
-find ./src/main/webapp/static/js/ -type f -exec sed -i "s/localhost:8080/${lb_dns}:80/g" {} +
-find ./src/main/webapp/static/js/ -type f -exec sed -i "s/localhost/${lb_dns}/g" {} +
+find ./src/main/webapp/static/js/ -type f -exec sed -i "s/localhost:8080/$lb_dns:80/g" {} +
+find ./src/main/webapp/static/js/ -type f -exec sed -i "s/localhost/$lb_dns/g" {} +
