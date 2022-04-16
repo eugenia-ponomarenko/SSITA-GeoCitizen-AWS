13ed7feb495bcd71f58d2954f037c1b5fa30ff93
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-16 23:01:51 +0300

    Update asg.tf

--- Terraform/asg/asg.tf
+++ Terraform/asg/asg.tf
@@ -10,8 +10,8 @@ resource "aws_launch_template" "web_tomcat" {
   }
   
   user_data = filebase64(templatefile("./user_data.tftpl", { 
-    nexus_user = "${var.nexus_user}"
-    nexus_password = "${var.nexus_password}" 
+    nexus_user = var.nexus_user
+    nexus_password = var.nexus_password
     } 
   )
   )
