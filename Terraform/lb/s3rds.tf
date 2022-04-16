e9dd9228a5c6f05862b614e8a4ed970dae3b4f19
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-16 13:39:48 +0300

    Update s3rds.tf

--- Terraform/lb/s3rds.tf
+++ Terraform/lb/s3rds.tf
@@ -17,8 +17,8 @@ resource "aws_db_instance" "GeoCitDB" {
   engine_version         = "12.9"
   instance_class         = "db.t2.micro"
   db_name                = "ss_demo_1"
-  username               = "postgres"
-  password               = "postgres"
+  username               = var.username
+  password               = var.password
   parameter_group_name   = "default.postgres12"
   skip_final_snapshot    = true
   vpc_security_group_ids = [aws_security_group.RDS_SecurityGroup.id]
