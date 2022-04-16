5bb0a542992a70831f6bd63ab41fc9863da62b3f
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-17 00:30:21 +0300

    Delete front-end directory

--- front-end/src/_validator/PasswordValidator.js
+++
@@ -1,5 +0,0 @@
-export const PASSWORD_PATTERN = /^(?=.*\d)(?=.*[a-zа-яіїє])(?=.*[A-ZА-ЯІЇЄ])(?=.*[-`!@#$%^&*()_+="'<>,./|\\?]).*$/
-
-export const PasswordValidator = (value, component) => {
-  return PASSWORD_PATTERN.test(value)
-}
--- front-end/src/components/form/SignInForm/SignInForm.vue
+++
@@ -1,7 +0,0 @@
-<template src="./index.html"></template>
-
-<props></props>
-
-<script src="./main.js"></script>
-
-<style scoped src="./style.scss"></style>
