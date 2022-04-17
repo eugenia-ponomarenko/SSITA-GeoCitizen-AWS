cd927c5a492823e5645e619a8ad183d15d04a61b
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-18 00:36:48 +0300

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -5,7 +5,7 @@
 #----------------------------------------------------------------------------------------------------
 ## Update email credentials
 
-. ./emailCredentials
+# . ./emailCredentials
 
 old_mail="[a-z0-9.]\{5,\}@gmail\.com"
 old_passwd="email.password=[A-Za-z0-9!@#$%^&*-]\{8,32\}"
@@ -15,20 +15,20 @@ sed -i "s/$old_mail/$email/g" ./src/main/resources/application.properties
 sed -i "s/$old_passwd/$new_passwd/g" ./src/main/resources/application.properties
 
 
-. ./Terraform/hosts
+# . ./Terraform/hosts
 
-#----------------------------------------------------------------------------------------------------
-# Fix application.properties
-sed -i -E \
-            "s/(http:\/\/localhost:8080)/http:\/\/$lb_dns:80/g; \
-            s/(postgresql:\/\/localhost:5432)/postgresql:\/\/$db_host/g;
-            s/(35.204.28.238:5432)/$db_host/g; " src/main/resources/application.properties
+# #----------------------------------------------------------------------------------------------------
+# # Fix application.properties
+# sed -i -E \
+#             "s/(http:\/\/localhost:8080)/http:\/\/$lb_dns:80/g; \
+#             s/(postgresql:\/\/localhost:5432)/postgresql:\/\/$db_host/g;
+#             s/(35.204.28.238:5432)/$db_host/g; " src/main/resources/application.properties
 
-#----------------------------------------------------------------------------------------------------
-# Repair index.html favicon
-sed -i "s/\/src\/assets/\.\/static/g" src/main/webapp/index.html
+# #----------------------------------------------------------------------------------------------------
+# # Repair index.html favicon
+# sed -i "s/\/src\/assets/\.\/static/g" src/main/webapp/index.html
 
-#----------------------------------------------------------------------------------------------------
-# Repair js bundles
-find ./src/main/webapp/static/js/ -type f -exec sed -i "s/localhost:8080/$lb_dns:80/g" {} +
-find ./src/main/webapp/static/js/ -type f -exec sed -i "s/localhost/$lb_dns/g" {} +
+# #----------------------------------------------------------------------------------------------------
+# # Repair js bundles
+# find ./src/main/webapp/static/js/ -type f -exec sed -i "s/localhost:8080/$lb_dns:80/g" {} +
+# find ./src/main/webapp/static/js/ -type f -exec sed -i "s/localhost/$lb_dns/g" {} +
