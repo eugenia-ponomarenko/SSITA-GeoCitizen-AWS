11c3119dae1b0d06e8fc1f8a6ee7d588afd1d5b3
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-18 12:30:33 +0300

    Update nexus_var.tf

--- Terraform/asg/nexus_var.tf
+++ Terraform/asg/nexus_var.tf
@@ -1,15 +1,12 @@
 variable "nexus_user" { 
     sensitive = true 
-    default = "${var.nexus_user}"
     type = string
 }
 variable "nexus_password" { 
     sensitive = true
-    default = "${var.nexus_password}"
     type = string
 }
 variable "nexus_url" { 
     sensitive = true
-    default = "${var.nexus_url}"
     type = string
 }
