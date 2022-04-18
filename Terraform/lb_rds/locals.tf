303640fbda684fec175f544515e42a32900d1173
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-18 13:24:52 +0300

    Update locals.tf

--- Terraform/lb_rds/locals.tf
+++ Terraform/lb_rds/locals.tf
@@ -1,12 +1,11 @@
 locals {
   eu_central_1ab            = ["subnet-0bd44ce4febe6eacb", "subnet-0f13e4432ca16d78a"]
-  lb_name                   = "TF-GeoCitizen-LB"
-  webserver_security_group  = "Ubuntu Security Group"
-  vpc_id                    = "vpc-008b05236db3e2d5e"
-  lb_security_group         = "GeoCitizen_LB"
-  postgres_security_group   = "Postgres SecurityGroup"
-  db_name                   = "PostgreSQL GeoCitizen"
-  bucket_name               = "geo-citizen-war"
+  lb_name                   = "test"
+  webserver_security_group  = "test"
+  vpc_id                    = "test"
+  lb_security_group         = "test"
+  postgres_security_group   = "test"
+  db_name                   = "test"
 }
 
 variable "psql_username" { 
