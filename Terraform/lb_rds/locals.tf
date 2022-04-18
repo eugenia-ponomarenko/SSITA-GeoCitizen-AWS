2608d072a66d15afa115e1b58cd6330801a1b44a
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-18 14:15:58 +0300

    Update locals.tf

--- Terraform/lb_rds/locals.tf
+++ Terraform/lb_rds/locals.tf
@@ -2,7 +2,7 @@ locals {
   eu_central_1ab            = ["subnet-0bd44ce4febe6eacb", "subnet-0f13e4432ca16d78a"]
   lb_name                   = "test"
   webserver_security_group  = "test"
-  vpc_id                    = "test"
+  vpc_id                    = "vpc-008b05236db3e2d5e"
   lb_security_group         = "test"
   postgres_security_group   = "test_psql"
   db_name                   = "test"
