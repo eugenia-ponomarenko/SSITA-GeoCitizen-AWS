90848bee09df2086585c15770b39e93ce0e667bb
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-18 13:13:39 +0300

    Update outputFiles.tf

--- Terraform/lb_rds/outputFiles.tf
+++ Terraform/lb_rds/outputFiles.tf
@@ -6,9 +6,11 @@ variable target_group_arn { default = "${aws_lb_target_group.citizen_tg.arn}" }
 EOF
 }
 resource "local_file" "hosts" {
-  filename = format("%s/%s", "../", "hosts")
+  filename = format("%s/%s", "../", "hosts.sh")
   file_permission   = "0600"
   content = <<EOL
-lb_dns="${aws_lb.tf_lb_webserver.dns_name}" db_host="${aws_db_instance.GeoCitDB.endpoint}" fix="fix"
+#!/bin/bash
+lb_dns="${aws_lb.tf_lb_webserver.dns_name}"
+db_host="${aws_db_instance.GeoCitDB.endpoint}"  
 EOL
 }
