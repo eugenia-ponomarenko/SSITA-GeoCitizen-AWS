d64efcf452d5da3d53cacdde5258203167e53f91
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-18 12:14:38 +0300

    Update asg.tf

--- Terraform/asg/asg.tf
+++ Terraform/asg/asg.tf
@@ -11,9 +11,9 @@ resource "aws_launch_template" "web_tomcat" {
   
   user_data = base64encode(
     templatefile("./user_data.tftpl", { 
-      nexus_user = ${var.nexus_user}
-      nexus_password = ${var.nexus_password}
-      nexus_url = ${var.nexus_url}
+      nexus_user = "${var.nexus_user}"
+      nexus_password = "${var.nexus_password}"
+      nexus_url = "${var.nexus_url}"
       }
     )
   )
