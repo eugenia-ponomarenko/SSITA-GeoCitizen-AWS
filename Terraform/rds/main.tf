7174dde49d4191b2c75c6d8032cf510cfcf232da
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-22 00:40:02 +0300

    Update main.tf

--- Terraform/rds/main.tf
+++ Terraform/rds/main.tf
@@ -13,3 +13,23 @@ resource "aws_db_instance" "GeoCitDB" {
     Name = local.db_name
   }
 }
+
+resource "aws_security_group" "RDS_SecurityGroup" {
+  name        = local.postgres_security_group
+  description = "GeoCitizen. SecurityGroup for RDS"
+
+  ingress {
+    description = "PostgreSQL"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
