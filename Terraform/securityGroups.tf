2ba812e10a455f60fba0f30e014114858952c5e5
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-04-22 00:39:47 +0300

    Update securityGroups.tf

--- Terraform/securityGroups.tf
+++ Terraform/securityGroups.tf
@@ -18,23 +18,3 @@ resource "aws_security_group" "ubuntuSecurityGroup" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-
-resource "aws_security_group" "RDS_SecurityGroup" {
-  name        = local.postgres_security_group
-  description = "GeoCitizen. SecurityGroup for RDS"
-
-  ingress {
-    description = "PostgreSQL"
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
